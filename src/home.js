a7ff07c4ede504d2e60e3f1e7446445f24b14253
aerobis <185216831+aerobis@users.noreply.github.com>
2026-02-10 16:58:07 +0545

    Added class-lists to dynamically created elements for home page.

--- src/home.js
+++ src/home.js
@@ -1,8 +1,14 @@
 document.addEventListener("DOMContentLoaded", (event)=>{
     let contentContainer = document.querySelector("#content");
+
     let introContainer = document.createElement('div');
+    introContainer.classList.add('intro-container');
+
     let introHeader = document.createElement('h1');
+    introHeader.classList.add('intro-header');
+
     let introText = document.createElement('p');
+    introText.classList.add('intro-text');
 
     introHeader.textContent = "Welcome to Our Restaurant";
     introText.textContent = "Experience amazing food and ambiance. Experienced staff, professional service, and above all, priority to a happy customer.";
