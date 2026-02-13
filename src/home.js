24daa002a59b39afa13d362cc4d79d02db0cf5a7
aerobis <185216831+aerobis@users.noreply.github.com>
2026-02-13 17:52:35 +0545

    Made style changes to the base home page.

--- src/home.js
+++ src/home.js
@@ -9,19 +9,25 @@ document.addEventListener("DOMContentLoaded", (event)=>{
     let backgroundImg = document.createElement('img');
     backgroundImg.src = backgroundImgPath;
     backgroundImg.alt = "An image of the cafe.";
+    backgroundImg.style.display = "block";
     backgroundImg.classList.add('background-image');
+
+    let introText = document.createElement('div');
+    introText.classList.add('intro-text');
 
     let introHeader = document.createElement('h1');
     introHeader.classList.add('intro-header');
 
-    let introText = document.createElement('p');
-    introText.classList.add('intro-text');
+    let introBody = document.createElement('p');
+    introText.classList.add('intro-body');
 
     introHeader.textContent = "Taste the Blend";
-    introText.textContent = "A good brew isn't just about the coffee, its about the brewer. Experience exquisite coffee, made by the best in the craft. First one's on us.";
+    introBody.textContent = "A good brew isn't just about the coffee, its about the brewer. Experience exquisite coffee, made by the best in the craft. First one's on us.";
+
+    introText.appendChild(introHeader);
+    introText.appendChild(introBody);
 
     introContainer.appendChild(backgroundImg);
-    introContainer.appendChild(introHeader);
     introContainer.appendChild(introText);
     
     contentContainer.appendChild(introContainer);
