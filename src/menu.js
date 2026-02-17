5323ce19e54f86a01a047a33aceb6009a7600138
aerobis <185216831+aerobis@users.noreply.github.com>
2026-02-17 19:44:43 +0545

    Centered menu by turning #content into a flex. There's space below the menu now.

--- src/menu.js
+++ src/menu.js
@@ -2,6 +2,11 @@ import {createMenuCategory} from "./menuCategory.js";
 
 import singleEspressoImg from "./assets/images/single-shot-espresso.jpg";
 import doubleEspressoImg from "./assets/images/double-shot-espresso.jpg";
+import cappuccinoImg from "./assets/images/cappuccino.jpg";
+import latteImg from "./assets/images/latte.jpg";
+import caramelLatteImg from "./assets/images/caramel-latte.jpg";
+import mochaImg from "./assets/images/mocha.jpg"
+
 
 export function renderMenu(contentContainer){
     if(contentContainer.children.length > 0){
@@ -27,13 +32,22 @@ export function renderMenu(contentContainer){
         {name: 'Espresso', image: singleEspressoImg, price:"Rs. 150"},
         {name: 'Doppio', image: doubleEspressoImg, price:"Rs. 250"},
     ];
+
+    let milkItems = [
+        {name: 'Cappuccino', image: cappuccinoImg, price: 'Rs. 260'},
+        {name: 'Cafe Latte', image: latteImg, price: 'Rs. 265'},
+        {name: 'Caramel Latte', image: caramelLatteImg, price: 'Rs. 285'},
+        {name: 'Mocha', image: mochaImg, price: 'Rs. 295'},
+    ];
     
     let espressoCategory = createMenuCategory("Espresso", espressoItems);
+    let milkCategory = createMenuCategory("Milk Items", milkItems);
 
     menuContainer.appendChild(menuLegend);
     menuContainer.appendChild(menuItemsContainer);
 
     menuItemsContainer.appendChild(espressoCategory);
+    menuItemsContainer.appendChild(milkCategory);
 
     contentContainer.appendChild(menuContainer);
 };
