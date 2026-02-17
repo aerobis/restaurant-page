c3e47e36063509101bb49744bda776bf600fc9c7
aerobis <185216831+aerobis@users.noreply.github.com>
2026-02-17 22:29:56 +0545

    Stylistic changes for the Menu

---
+++ src/about.js
@@ -0,0 +1,10 @@
+export function renderAbout(contentContainer){
+    if(contentContainer.children.length > 0){
+        contentContainer.innerHTML = '';
+    }
+
+    let aboutContainer = document.createElement('div');
+    aboutContainer.className = "about-container";
+
+    
+};
--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import "./style.css";
 import {renderHome} from "./home.js";
 import {renderMenu} from "./menu.js";
+import {renderAbout} from "./about.js";
 
 document.addEventListener("DOMContentLoaded", (event)=>{
     let contentContainer = document.querySelector("#content");
     let homeBtn = document.querySelector("#home-button");
     let menuBtn = document.querySelector("#menu-button");
+    let aboutBtn = document.querySelector("#about-button");
 
     homeBtn.addEventListener("click", ()=>{
         renderHome(contentContainer);
@@ -13,6 +15,10 @@ document.addEventListener("DOMContentLoaded", (event)=>{
 
     menuBtn.addEventListener("click", ()=>{
         renderMenu(contentContainer);
+    });
+
+    aboutBtn.addEventListener("click", ()=>{
+        renderAbout(contentContainer);
     });
 
     renderMenu(contentContainer);
