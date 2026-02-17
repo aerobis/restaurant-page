031f5714de7457505e81e0e206c03f68f4186b7b
aerobis <185216831+aerobis@users.noreply.github.com>
2026-02-17 22:44:36 +0545

    Made base framework for About section.

--- src/about.js
+++ src/about.js
@@ -6,5 +6,17 @@ export function renderAbout(contentContainer){
     let aboutContainer = document.createElement('div');
     aboutContainer.className = "about-container";
 
-    
+    let aboutHeadingSection = document.createElement('div');
+    aboutHeadingSection.className = "about-heading-section";
+    let aboutHeading = document.createElement('h3');
+    aboutHeading.textContent = "ABOUT US";
+    aboutHeading.className = "about-heading";
+    aboutHeadingSection.appendChild(aboutHeading);
+
+    let aboutContent = document.createElement('div');
+    aboutContent.className = "about-content-section";
+
+    aboutContainer.appendChild(aboutHeadingSection);
+    aboutContainer.appendChild(aboutContent);
+    contentContainer.appendChild(aboutContainer);
 };
