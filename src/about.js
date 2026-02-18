2c82428eef9cd06605448a15c9299caff8982f99
aerobis <185216831+aerobis@users.noreply.github.com>
2026-02-18 20:34:36 +0545

    Finished the about section.

--- src/about.js
+++ src/about.js
@@ -18,13 +18,32 @@ export function renderAbout(contentContainer){
     let aboutContent = document.createElement('div');
     aboutContent.className = "about-content-section";
 
+    let welcome = [
+        `We are a local cafe based in Abc, Def, focused on bringing the exquisite and authentic taste of pure blend coffee. We get our beans locally sourced where we can from
+        the highest quality farms, and where we can't, from brands internationally recognized for delivering the best taste. Our baristas are seasoned veterans in the field
+        of the Cafe, and are dedicated to elevate your coffee game further.`,
+        `We even have a special game for true coffee enthusiasts, where managing to figure out the flavors from a coffee bean rewards discounts, or even hamper prizes.`,
+        `Our cakes, similarly, are also made with love from authentic dairy, and with baking so gentle it'll remind even the fondest of their first dessert.`,
+        `Join us for an evening. Or a morning. Your call.`,
+    ];
+
     let timings = [
         "Sunday - Thursday: 9am - 6pm",
         "Friday - Saturday: 11am - 4pm"
     ];
 
-    let timingSection = createSection("Timings", timings);
+    let location = [
+        `We are located at Abc, Def.`,
+    ];
+
+    let welcomeSection = createSection("Welcome", welcome);
+    aboutContent.appendChild(welcomeSection);
+
+    let timingSection = createSection("Timing", timings);
     aboutContent.appendChild(timingSection);
+
+    let locationSection = createSection("Visit Us", location);
+    aboutContent.appendChild(locationSection);
 
     aboutContainer.appendChild(aboutLegendSection);
     aboutContainer.appendChild(aboutContent);
